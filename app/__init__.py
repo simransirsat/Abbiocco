3397044b743daf89c553f4c9ada69023b0433f0c
Rahul <rahul.keluskar@sap.com>
2021-06-07 20:02:09 +0530

    Added changes in setup and readme for setup

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 app = Flask(__name__)
-app.config.from_object(config_class)
+app.config.from_object(Config)
 bootstrap=Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
--- migrations/versions/56a0c4195513_.py
+++ migrations/versions/56a0c4195513_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: ab48a875fa2f
+Revision ID: 56a0c4195513
 Revises: 
-Create Date: 2019-09-18 20:01:31.829364
+Create Date: 2021-06-07 19:39:57.236707
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'ab48a875fa2f'
+revision = '56a0c4195513'
 down_revision = None
 branch_labels = None
 depends_on = None
