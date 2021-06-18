7954536ca1f176b554d5a5ff80009260f52f0648
Rahul <rahul.keluskar@sap.com>
2021-06-18 19:16:37 +0530

    Fix: altered the size of recipe name

--- app/models.py
+++ app/models.py
@@ -57,7 +57,7 @@ class Recipe(db.Model):
 
 	recipe_id = db.Column(db.String(64),nullable=False, primary_key=True)
 	recipe_name = db.Column(db.String(200), nullable=True)
-	img_url = db.Column(db.String(50000),nullable = True)
+	img_url = db.Column(db.String(5000),nullable = True)
 	instructions = db.Column(db.String(5000), nullable=True)
 	user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
 
