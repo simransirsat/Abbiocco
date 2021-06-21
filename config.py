3ba9acd2da0529b9c0df968a724a5a8a64c1d153
wayneremedios <wayner199922@gmail.com>
2021-06-21 15:30:40 +0530

    Index page changes

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config(object):
 	SECRET_KEY=os.environ.get('SECRET_KEY') or 'Hard coded string'
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
 	'sqlite:///' + os.path.join(basedir, 'app.db')
+	# SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:rootpassword@localhost:3306/salubreata"
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 
