c66ed3921064232952705813e3279e845feec3d2
Rahul <rahul.keluskar@sap.com>
2021-07-14 10:02:02 +0530

    fixed config error

--- salubreata.py
+++ salubreata.py
@@ -1,6 +1,7 @@
 from app import app, db
 from app.models import User
 
+
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User}
@@ -10,5 +11,5 @@ def some_other_function():
     pass
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
